ee08af46056f410697b5791469d1027e6f06a65c
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-08-28 10:50:52 +0800

    Level-0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Zoro User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Zoro.java
@@ -0,0 +1,9 @@
+public class Zoro {
+    public static void main(String[] args) {
+        String name = "Zoro";
+        System.out.println("________________________");
+        System.out.println("Hello from, I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("________________________");
+    }
+}
