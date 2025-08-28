90dde4186eb3374694ca706e61be3101b4bebf4c
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-08-28 13:49:57 +0800

    Add bounds check for marking tasks

--- src/main/java/Zoro.java
+++ src/main/java/Zoro.java
@@ -44,9 +44,14 @@ public class Zoro {
     }
 
     private static void _markTask(Task[] task_list, int task_list_index, String user_input) {
+        int mark_idx = Integer.parseInt(user_input.split(" ")[1]);
+        if (mark_idx < 0 || mark_idx > task_list_index) {
+            System.out.println("Invalid task ID");
+            return;
+        }
         //if the user types mark 5 - split the string into ["mark", "5"]
         //cast the "5" to int and use it as index into task_list to set task to done
-        if (task_list[Integer.parseInt(user_input.split(" ")[1])].setDone()) {
+        if (task_list[mark_idx].setDone()) {
             System.out.println("Task " + user_input + " has been marked as done!");
         } else {
            System.out.println("Task " + user_input + " has been unmarked!");
@@ -55,7 +60,7 @@ public class Zoro {
     }
 
     public static void storeList(Scanner scanner, String user_input) {
-       System.out.println("Storing a list for you!\nType something and I will store it!\nType \"list\" to see what you stored!");
+       System.out.println("Storing a list for you!\nType something and I will store it.\nType \"list\" to see what you stored\nType \"mark [x]\" and I will mark/unmark that task.\nType \"bye\" to exit!");
         Task[] task_list = new Task[100];
         int task_list_index = 0;
         user_input = scanner.nextLine();
@@ -83,7 +88,7 @@ public class Zoro {
         printIntro();
         try (Scanner scanner = new Scanner(System.in)) {
             String user_input = scanner.nextLine();
-            if (user_input.toLowerCase().equals("echo")) {
+            if (user_input.toLowerCase().split(" ")[0].equals("echo")) {
                 echo(scanner, user_input);
             } else if (user_input.toLowerCase().replace(" ", "").equals("storelist")) {
                 storeList(scanner, user_input);
