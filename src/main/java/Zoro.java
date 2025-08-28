fc3dd760eabd4d593f699e2236dc63ad4ed49507
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-08-28 11:15:35 +0800

    Echo

--- src/main/java/Zoro.java
+++ src/main/java/Zoro.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class Zoro {
     public static void main(String[] args) {
         String name = "Zoro";
         System.out.println("________________________");
-        System.out.println("Hello from, I'm " + name);
+        System.out.println("Hello from, I'm " + name + " - I think I'm lost..");
         System.out.println("What can I do for you?");
         System.out.println("________________________");
+        try (Scanner scanner = new Scanner(System.in)) {
+            String userInput = scanner.nextLine();
+            while (!(userInput.toLowerCase().equals("bye"))) {
+                System.out.println(userInput);
+                userInput = scanner.nextLine();
+            }
+            System.out.println("Goodbye - see you soon");
+        }
     }
 }
