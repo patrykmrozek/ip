569324f8b30e1109a740e32d1cbe09e3e876442b
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2025-08-31 19:15:11 +0800

    Add printIntro method to UserInterface

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class UserInterface {
+    public void printIntro() {
+        String ZORO_LOGO =
+            "╔═════════════════════════════════════╗\n" +
+            "║  ███████╗ ██████╗ ██████╗  ██████╗  ║\n" +
+            "║  ╚══███╔╝██╔═══██╗██╔══██╗██╔═══██╗ ║\n" +
+            "║    ███╔╝ ██║   ██║██████╔╝██║   ██║ ║\n" +
+            "║   ███╔╝  ██║   ██║██╔══██╗██║   ██║ ║\n" +
+            "║  ███████╗╚██████╔╝██║  ██║╚██████╔  ║\n" +
+            "║  ╚══════╝ ╚═════╝ ╚═╝  ╚═╝ ╚═════╝  ║\n" +
+            "╚═════════════════════════════════════╝";
+        System.out.println(ZORO_LOGO);
+        String name = "Zoro";
+        System.out.println("________________________");
+        System.out.println("Hello, I'm " + name + " - I think I'm lost..");
+        System.out.println("What can I do for you?");
+        System.out.println(
+            "1: echo\n" +
+            "2: store list\n" +
+            "More features coming soon!\n" +
+            "Type \"bye\" or \"exit\" to exit"
+        );
+        System.out.println("________________________\n");
+    }
 }
